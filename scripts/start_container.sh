aa6a89a22a45d690cbbc7878e5117cbff43a82f4
Vijayaragavan C <54120708+vijaytech2@users.noreply.github.com>
2023-08-11 11:24:58 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull cvragavan/simple-python-flash-app:latest
+docker pull cvragavan/tomcat9cust:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 cvragavan/simple-python-flash-app
+docker run -d -p 80:80 cvragavan/tomcat9cust
